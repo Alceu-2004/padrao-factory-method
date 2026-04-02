9ebad7fcb220c10c2c0dc829e4fa8ffa148a91cd
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-02 10:57:55 -0300

    feat: adiciona mensagem inicial na execução do sistema

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("=== Sistema de Veículos ===");
+
         VeiculoFactory factory;
 
         factory = new CarroFactory();
